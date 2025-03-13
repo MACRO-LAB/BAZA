2056a29c38ddb65350368076f496b2ea5d5a2ddb
VIKTOR_TIMOFEEV <macrolabs@mail.ru>
2025-03-13 17:09:58 +0800

    learn selfedu

--- PYTHON/learning from selfedu/oop/ex30.py
+++ PYTHON/learning from selfedu/oop/ex30.py
@@ -1,2 +1,18 @@
 """#30. Распространение исключений (propagation exceptions) | ООП Python
 """
+
+def func2():
+    return 1/0
+
+def func1():
+    try:
+        func2()
+    except ZeroDivisionError:
+        print("asdfs")
+
+print("random")
+
+func1()
+
+print("random2")
+print("randome")
