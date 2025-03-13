439d59e69b4cfddd25a681c1b0abbbe1375b2902
VIKTOR_TIMOFEEV <macrolabs@mail.ru>
2025-03-13 09:12:23 +0800

    learn selfedu

--- PYTHON/learning from selfedu/oop/ex22.py
+++ PYTHON/learning from selfedu/oop/ex22.py
@@ -5,7 +5,8 @@
 
 class Geom:
     name = "Geom"
-    def __init__(self,x1,y1,x2,y2):
+
+    def __init__(self, x1, y1, x2, y2):
         print(f"Инициализатор ГЕом для {self.__class__}")
         self.x1 = x1
         self.y1 = y1
@@ -14,20 +15,24 @@ class Geom:
 
 
 class Line(Geom):
-    def draw (self):
+    def draw(self):
         print("Рисование линии")
 
 
 class Rect(Geom):
-    def __init__(self,x1,y1,x2,y2, fill=None  ):
+
+
+class Rect(Geom):
+    def __init__(self, x1, y1, x2, y2, fill=None):
         # Geom.__init__(self,x1,y1,x2,y2)
-        super().__init__(x1,y1,x2,y2) # аналог строки выше #Делинирование
+        super().__init__(x1, y1, x2, y2)  # аналог строки выше #Делинирование
         self.fill = fill
         print("инициация Rect")
 
-    def draw (self):
+    def draw(self):
         print("Рисование Rect")
 
-l = Line(0,0, 10,20   )
-r = Rect(1,2,3,4)
-print(l.__dict__ , r.__dict__, sep="\n")
+
+l = Line(0, 0, 10, 20)
+r = Rect(1, 2, 3, 4)
+print(l.__dict__, r.__dict__, sep="\n")
