0334adebb4ddb1760e065b24c39f738e0334ee9e
VIKTOR_TIMOFEEV <macrolabs@mail.ru>
2025-03-15 21:27:15 +0800

    Python Hub Studio Асинхронные функции

---
+++ PYTHON/asyncio/ex5.py
@@ -0,0 +1,30 @@
+"""
+Библиотеки асинхронные:
+    aiohttp
+    aiogram
+    aiosqlite
+"""
+import asyncio
+from time import time
+
+
+
+async def do_smht(sec):
+    await asyncio.sleep(sec)
+    print("fesult ", sec)
+
+async def print1(sec):
+    await asyncio.sleep(sec)
+    print(sec)
+    await do_smht(sec)
+
+
+async def main():
+    async  with asyncio.TaskGroup() as tg:
+        for i in range(1, 16):
+            tg.create_task(print1(i))
+
+
+start = time()
+asyncio.run(main())
+print(f"Время на работу {time() - start}")
